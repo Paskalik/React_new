838b40493cc8c4bfb8ed1f9e6a0184b7d904be58
Paskalik <57663523+Paskalik@users.noreply.github.com>
2020-05-27 20:40:22 +0300

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React_new
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,73 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const movie = {
+  title: "Avengers: Infinity War",
+  vote_average: 8.5,
+  image: "https://image.tmdb.org/t/p/w500/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
+  overview:
+    "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle"
+};
+
+function Image(props) {
+  //  console.log("Image props", props);
+  return <img src={props.src} alt={props.alt} />;
+}
+
+//function MovieItem(props) {
+//  console.log("MovieItem props", props);
+//  const {
+//    data: { title, vote_average, image }
+//  } = props;
+//  return (
+//    <div>
+//      <Image src={image} alt={title} />
+//      <p>{title}</p>
+//      <p>{vote_average}</p>
+//    </div>
+//  );
+//}
+
+class MovieItem extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      show: false
+    };
+  }
+
+  toggleOverview = () => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
+
+  render() {
+    const {
+      data: { title, vote_average, image, overview }
+    } = this.props;
+    console.log("state", this.state);
+    return (
+      <div>
+        <Image src={image} alt={title} />
+        <p>{title}</p>
+        <p>{vote_average}</p>
+        <button type="button" onClick={this.toggleOverview}>
+          {this.state.show ? "hide" : "show"}
+        </button>
+        {this.state.show ? <p>{overview}</p> : null}
+      </div>
+    );
+  }
+}
+
+function App() {
+  return (
+    <div>
+      <MovieItem data={movie} />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
